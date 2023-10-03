47f9b1c59f0bf96c8327525e3485c550f13ed4bd
Ismael Soares Doria <ismaelsoaresdoria@gmail.com>
2023-10-03 18:38:23 -0300

    Initial commit react-form-advanced using react-hook-forms and zod

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import './styles/global.css';
+
+import { useForm } from 'react-hook-form';
+
+export function App() {
+  const [output, setOutput] = useState('')
+  const { register, handleSubmit } = useForm()
+
+  function createUser(data: any) {
+    setOutput(JSON.stringify(data, null, 2))
+  }
+
+  //High Order Function
+  return (
+    <main className='h-screen bg-zinc-950 flex text-zinc-300 flex flex-col gap-10 items-center justify-center'>
+      <form
+        onSubmit={handleSubmit(createUser)}
+        className='flex flex-col gap-4 w-full max-w-xs'
+      >
+        <div className='flex flex-col gap-1'>
+          <label htmlFor="">E-mail</label>
+          <input
+            type="email"
+            className='border border-zinc-600 shadow-sm rounded h-10 px-3 bg-zinc-900 text-white'
+            {...register('email')}
+          />
+        </div>
+
+        <div className='flex flex-col gap-1'>
+          <label htmlFor="">Senha</label>
+          <input
+            type="password"
+            className='border border-zinc-600 shadow-sm rounded h-10 px-3 bg-zinc-900 text-white'
+            {...register('password')}
+          />
+        </div>
+        <button
+          type="submit"
+          className='bg-emerald-500 rounded font-semibold text-white h-10 hover:bg-emerald-600'
+        >Salvar</button>
+
+      </form>
+      <pre>{output}</pre>
+    </main>
+  )
+}
+
